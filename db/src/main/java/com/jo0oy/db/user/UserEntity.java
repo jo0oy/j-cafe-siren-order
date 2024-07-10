4e4f66413273771b3c5191f1715ea3c65badb4d1
jo0oy <rkfkrh07@naver.com>
2024-07-10 18:40:48 +0900

    [feat] 회원 엔티티 및 레포지토리 구현
    - db 모듈에 Spring Data JPA를 사용한 UserEntity 및 UserRepository 구현
    
    related to #3

---
+++ db/src/main/java/com/jo0oy/db/user/UserEntity.java
@@ -0,0 +1,40 @@
+package com.jo0oy.db.user;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "user")
+@Entity
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 100, unique = true, nullable = false)
+    private String username;
+
+    @Column(length = 150, nullable = false)
+    private String password;
+
+    @Column(length = 30, nullable = false)
+    private String phoneNumber;
+
+    @Builder
+    private UserEntity(
+        Long id,
+        String username,
+        String password,
+        String phoneNumber
+    ) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+    }
+}
---
+++ db/src/main/java/com/jo0oy/db/user/UserRepository.java
@@ -0,0 +1,6 @@
+package com.jo0oy.db.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+}
