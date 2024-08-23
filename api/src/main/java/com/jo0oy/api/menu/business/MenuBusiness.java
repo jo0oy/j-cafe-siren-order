d8a06345da39f0f2d0d2715eb9529e2781752b49
jo0oy <rkfkrh07@naver.com>
2024-08-24 02:08:04 +0900

    [feat] 비즈니스 로직 수행하는 MenuBusiness 구현
    - 기본 CRUD 기능 구현
    
    related to #8

---
+++ api/src/main/java/com/jo0oy/api/menu/business/MenuBusiness.java
@@ -0,0 +1,39 @@
+package com.jo0oy.api.menu.business;
+
+import com.jo0oy.api.global.annotation.Business;
+import com.jo0oy.api.menu.converter.MenuConverter;
+import com.jo0oy.api.menu.dto.request.MenuRequest;
+import com.jo0oy.api.menu.dto.response.MenuResponse;
+import com.jo0oy.api.menu.service.MenuService;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Business
+public class MenuBusiness {
+
+    private final MenuService menuService;
+    private final MenuConverter menuConverter;
+
+    public void register(MenuRequest request) {
+        var menuEntity = menuConverter.toEntity(request);
+        menuService.save(menuEntity);
+    }
+
+    public MenuResponse getMenuById(String menuId) {
+        var menuEntity = menuService.findById(menuId);
+        return menuConverter.toResponse(menuEntity);
+    }
+
+    public MenuResponse update(String menuId, MenuRequest request) {
+        var updateRequestEntity = menuConverter.toEntity(request);
+        updateRequestEntity.setId(menuId);
+
+        var updatedEntity = menuService.update(updateRequestEntity);
+
+        return menuConverter.toResponse(updatedEntity);
+    }
+
+    public void delete(String menuId) {
+        menuService.delete(menuId);
+    }
+}
