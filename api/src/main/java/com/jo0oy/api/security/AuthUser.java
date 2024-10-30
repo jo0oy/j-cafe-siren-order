1c1af55859da300391cb14d0789b9c89add90ecc
jo0oy <rkfkrh07@naver.com>
2024-10-31 02:45:02 +0900

    [feat] 시큐리티 UserDetails 구현체 AuthUser 추가
    related to #12

---
+++ api/src/main/java/com/jo0oy/api/security/AuthUser.java
@@ -0,0 +1,34 @@
+package com.jo0oy.api.security;
+
+import com.jo0oy.db.user.jpa.UserEntity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+public class AuthUser implements UserDetails {
+
+    private final UserEntity user;
+
+    public AuthUser(UserEntity user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return this.user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return this.user.getUsername();
+    }
+
+}
