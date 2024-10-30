34931016da8ef5712faac5f493ea2e8226f95f81
jo0oy <rkfkrh07@naver.com>
2024-10-31 02:46:05 +0900

    [feat] 단일 회원 조회 by username 구현
    related to #12

--- db/src/main/java/com/jo0oy/db/user/jpa/UserRepository.java
+++ db/src/main/java/com/jo0oy/db/user/jpa/UserRepository.java
@@ -2,5 +2,9 @@ package com.jo0oy.db.user.jpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+    Optional<UserEntity> findByUsername(String username);
 }
